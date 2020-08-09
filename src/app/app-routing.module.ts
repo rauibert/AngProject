187245fcbbc374edcecd106c91803df0ff90653a
Rauibert <rauibertnf@gmail.com>
2020-08-09 20:24:42 +0200

    Creación de componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    CreateComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
