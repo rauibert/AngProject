6160ec4106b8b004d4ce8b21644ea2080fddb5c7
Rauibert <rauibertnf@gmail.com>
2020-08-19 21:45:34 +0200

    Subir imagenes

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/projects';
 import { ProjectService } from '../../services/project.service';
+import { UploadService } from '../../services/upload.service';
+import { Global } from '../../services/global';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
   ) {
     this.title = "Crear nuevo proyecto";
     this.project = new Project('', '', '', '', 2020, '', '' );
@@ -24,11 +28,18 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit(form){
-    console.log(this.project);
+    //Guardo los datos
     this._projectService.saveProject(this.project).subscribe(
       response => {
         if(response.project){
-          this.status = 'success';
+          //Subo la imagen
+          this._uploadService.makeFileRequest(Global.url + 'uploadImage/' + response.project._id, [], this.filesToUpload, 'image' )
+          .then((result:any) => {
+            console.log(result);
+            this.status = 'success';
+            form.reset();
+          });
+
         }else{
           this.status = 'failed';
         }
@@ -37,6 +48,10 @@ export class CreateComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
