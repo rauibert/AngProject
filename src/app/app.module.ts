e475c1ae67d776a9f6d815f3786eee8d1a456b34
Rauibert <rauibertnf@gmail.com>
2020-08-25 22:18:36 +0200

    Web detalle proyectos I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { CreateComponent } from './components/create/create.component';
 import { ErrorComponent } from './components/error/error.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ErrorComponent } from './components/error/error.component';
     ProjectsComponent,
     ContactComponent,
     CreateComponent,
-    ErrorComponent
+    ErrorComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/detail/detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/projects';
+import { ProjectService } from '../../services/project.service';
+import { Global } from '../../services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css'],
+  providers: [ProjectService]
+})
+export class DetailComponent implements OnInit {
+  public url: string;
+
+  constructor(
+    private _projectService : ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) { 
+    this.url = Global.url;
+  }
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      let id = params.id;
+      this.getProject(id);
+    });
+  }
+
+  getProject(id){
+
+  }
+
+}
