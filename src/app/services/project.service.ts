8b133e3a2f098a05b6e21ff7e7571bf209fc7a00
Rauibert <rauibertnf@gmail.com>
2020-08-30 00:22:34 +0200

    Borrar proyecto en página detalle

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -36,4 +36,10 @@ export class ProjectService{
 
         return this._http.get(this.url + 'project' +id, {headers: headers});
     }
+
+    deleteProject(id):Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.delete(this.url + 'project/' +id, {headers: headers});
+    }
 }
